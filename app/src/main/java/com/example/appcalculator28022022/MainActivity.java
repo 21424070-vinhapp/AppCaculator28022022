fc4b90d98c2a6964bf5f56c264080ed8e40fac7f
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-02-28 16:05:15 +0700

    on lai Log.d

--- app/src/main/java/com/example/appcalculator28022022/MainActivity.java
+++ app/src/main/java/com/example/appcalculator28022022/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appcalculator28022022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
         mEdtNumber2=findViewById(R.id.editTextNumber1);
         mTvResult=findViewById(R.id.textViewResult);
         mBtnPlus=findViewById(R.id.btnPlus);
+        mBtnMinus=findViewById(R.id.btnMinus);
+        mBtnMultiplication=findViewById(R.id.btnMultiplication);
+        mBtnDivision=findViewById(R.id.btnDivision);
+
+        //Ham kiem tra gia tri
+        int a=5;
+        Log.d("BBB",String.valueOf(a));
     }
 }
